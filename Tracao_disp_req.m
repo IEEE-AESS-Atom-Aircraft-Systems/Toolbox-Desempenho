a859a4f627b2afc1ba0225d8db4efbcc360f4755
AndreMarques2002 <andre-msilva2002@hotmail.com>
2021-10-01 20:40:33 -0300

    Versão 1.0

---
+++ Tracao_disp_req.m
@@ -0,0 +1,41 @@
+%Tração Disponível x Tração Requerida 
+
+clear
+clc
+
+prompt = {'Coeficiente de Oswald','Alongamento',...
+          'Densidade do ar (kg/m³)','Aceleração da gravidade (m/s²)',...
+          'Massa total (kg)','Área da asa (m²)',...
+          'Coeficiente de arrasto mínimo',...
+          'Coeficiente de sustentação-arrasto mínimo'};
+
+default = {'0.75','20','1.225','9.80665','1.21','0.6','0.0038','0.2'};
+rc = [1,40; 1,40; 1,40; 1,40; 1,40; 1,40; 1,40; 1,40];
+dims = inputdlg (prompt, 'Preencha com os dados do VANT', rc, default);
+
+if (isempty (dims))
+  helpdlg ('Preencha corretamente', 'Trações Disponível e Requerida');
+else
+  Td = [224.3389 17.5144 17.2055 16.7392 16.1235 15.3641 14.4639 13.4256 12.2456 10.9221 9.4538 7.8755 6.1184 4.2520 2.3635 0.7664];
+  v = [0:2:30];
+
+%Polar de arrasto para perfil arqueado (cumbered)
+  k = 1 / (pi * str2num (dims{1}) * str2num (dims{2}));
+  V = [0.01:0.01:30];
+  Cl = ((2 * str2num (dims{4}) * str2num (dims{5})) ./ (str2num (dims{3}) * str2num (dims{6}).*V.^2));
+  Cd = str2num (dims{7}) + k * (str2num (dims{8}) - Cl).^2;
+  Tr = (str2num (dims{4}) * str2num (dims{5})) ./ (Cl ./ Cd);
+
+  hold on
+  plot (v,Td,'-g');
+  grid on
+  plot (V,Tr,'-b');
+  title('Tração Disponível x Tração Requerida');
+  xlabel('Velocidade do escoamento livre V (m/s)');
+  ylabel('Tração (N)');
+  ylim([-2 , 220])
+  xlim([-2 , 32])
+  legend('Tração Disponível', 'Tração Requerida');
+  
+  helpdlg (sprintf ('O gráfico das Trações Disponível e Requerida foi plotado.'), 'Trações Disponível e Requerida');
+endif
